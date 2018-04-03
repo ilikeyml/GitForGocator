f7c908b24e0787390e007516550f1e8a9485f331
Zhou Peng <ilikeyml@hotmail.com>
2018-04-03 09:57:20 +0800

    TestForm

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageBoxEx1 = new LMI.Gocator.Tools.ImageBoxEx();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageBoxEx1
+            // 
+            this.imageBoxEx1.Image = ((System.Drawing.Image)(resources.GetObject("imageBoxEx1.Image")));
+            this.imageBoxEx1.InvertMouse = true;
+            this.imageBoxEx1.Location = new System.Drawing.Point(57, 51);
+            this.imageBoxEx1.Name = "imageBoxEx1";
+            this.imageBoxEx1.Size = new System.Drawing.Size(424, 378);
+            this.imageBoxEx1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(564, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(551, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "None";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(587, 323);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "rect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 501);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBoxEx1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LMI.Gocator.Tools.ImageBoxEx imageBoxEx1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            imageBoxEx1.MouseClick += ImageBoxEx1_MouseClick;
+            
+        }
+
+        private void ImageBoxEx1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(imageBoxEx1.IsPointInImage(e.Location))
+            {
+                Point p;
+                p = imageBoxEx1.PointToImage(e.Location);
+                button1.Text = p.ToString();
+
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            imageBoxEx1.SelectionColor = Color.Blue;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            imageBoxEx1.SelectionMode = LMI.Gocator.Tools.ImageBoxSelectionMode.None;
+            imageBoxEx1.SelectionRegion = RectangleF.Empty;
+    
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            imageBoxEx1.SelectionMode = LMI.Gocator.Tools.ImageBoxSelectionMode.Rectangle;
+         
+        }
+    }
+}
